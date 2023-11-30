71ed526f0b74d9de74733e7b0674a94cfa91337e
Yannick Wagner <yannickAlexander@gmx.de>
2023-11-30 05:05:42 +0100

    feat: From implementation with necessary casts for primitive types to Float<F>

--- src/lib.rs
+++ src/lib.rs
@@ -17,13 +17,6 @@ pub struct Float<F>(pub F)
 where
     F: Clone;
 
-// impl<F> Float<F>
-// where
-//     F: Clone,
-// {
-//     pub const EPSILON: Self = Self(1e-12);
-// }
-
 impl<F> std::fmt::Debug for Float<F>
 where
     F: Clone + std::fmt::Debug,
@@ -44,25 +37,25 @@ where
 
 impl<F, T> std::cmp::PartialEq<T> for Float<F>
 where
-    F: Clone + PartialEq + ::num_traits::NumCast,
-    T: Clone + ::num_traits::NumCast,
+    F: Clone + PartialEq + num_traits::NumCast,
+    T: Clone + num_traits::NumCast,
 {
     fn eq(&self, other: &T) -> bool {
-        self.0.eq(&::num_traits::cast(other.clone()).unwrap())
+        self.0.eq(&num_traits::cast(other.clone()).unwrap())
     }
 }
 
 #[cfg(feature = "eq")]
-impl<F> Eq for Float<F> where F: ::num_traits::NumCast + Clone + PartialEq {}
+impl<F> Eq for Float<F> where F: num_traits::NumCast + Clone + PartialEq {}
 
 impl<F, T> std::cmp::PartialOrd<T> for Float<F>
 where
-    F: Clone + PartialOrd + ::num_traits::NumCast,
-    T: Clone + ::num_traits::NumCast,
+    F: Clone + PartialOrd + num_traits::NumCast,
+    T: Clone + num_traits::NumCast,
 {
     fn partial_cmp(&self, other: &T) -> Option<std::cmp::Ordering> {
         self.0
-            .partial_cmp(&::num_traits::cast(other.clone()).unwrap())
+            .partial_cmp(&num_traits::cast(other.clone()).unwrap())
     }
 }
 
@@ -70,19 +63,23 @@ where
 #[allow(clippy::derive_ord_xor_partial_ord)]
 impl<F> Ord for Float<F>
 where
-    F: ::num_traits::NumCast + Clone + PartialOrd,
+    F: num_traits::NumCast + Clone + PartialOrd,
 {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.partial_cmp(other).unwrap()
     }
 }
 
+// num_traits::Bounded is intentionally not implemented for Float<F>
+// to avoid the From<Float<T>> for Float<F> implementation which would conflict
+// with the standard library.
 #[allow(clippy::fallible_impl_from)]
-impl<T> From<T> for Float<T>
+impl<T, F> From<T> for Float<F>
 where
-    T: ::num_traits::NumCast + Clone,
+    T: num_traits::NumCast + num_traits::Bounded,
+    F: num_traits::NumCast + Clone,
 {
     fn from(value: T) -> Self {
-        ::num_traits::cast(value).unwrap()
+        num_traits::cast(value).unwrap()
     }
 }
